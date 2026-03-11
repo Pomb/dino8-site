33aee3d28d35feab6627c92b8b67da8b10911b29
Paul Lombard <pmlomb@gmail.com>
2026-03-11 23:27:37 +0200

    deploy site update

---
+++ demo/conf.lua
@@ -0,0 +1,11 @@
+-- conf.lua
+return {
+	title = "demo",
+	description = "A Dino8 game",
+	author = "",
+	version = "0.1.0",
+	width = 128,
+	height = 128,
+	scale = 4,
+	entry = "main.d8"
+}
---
+++ demo/lib/utils.lua
@@ -0,0 +1,8 @@
+-- Utility functions module
+local utils = {}
+
+function utils.hello()
+	return "Hello, World!"
+end
+
+return utils
